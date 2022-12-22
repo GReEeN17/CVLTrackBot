2afa723950c308bcea07b71274c2e70e6f18e57d
Aleksandr <azelen04@mail.ru>
2022-12-22 22:13:44 +0300

    Made some plot for future database

--- database.py
+++ database.py
@@ -17,3 +17,29 @@ class Database:
         cursor.execute('''CREATE TABLE table (id INT PRIMARY KEY, place INT, command TEXT, points INT)''')
         connection.commit()
         cursor.close()
+
+    def connection(self):
+        db_path = os.path.join(os.getcwd(), f"{self.name}.db")
+        if not os.path.exists(db_path):
+            self.create_db()
+        return sqlite3.connect(f"{self.name}.db")
+
+
+    def execute_query(self, query, select=False):
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        if select:
+            records = cursor
+        self.conn.commit()
+        cursor.close()
+
+
+    def selectLG(self, game_id):
+        select_query = f"""SELECT * FROM league_games WHERE id={game_id}"""
+
+
+
+    def insertLG(self, league_games):
+        for i, game in enumerate(league_games):
+            insert_query = f"""INSERT"""
+
